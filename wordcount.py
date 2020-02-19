c235f9ee1f3db0fcf30c4911902a087b8da10959
safia88 <safialiusa@gmail.com>
2020-02-19 11:34:06 +0600

    word count done

--- wordcount.py
+++ wordcount.py
@@ -53,6 +53,45 @@ import sys
 # calls the print_words() and print_top() functions which you must define.
 
 
+def word_count_dict(filename):
+    word_count = {}  # Map each word to its count
+    input_file = open(filename, 'r')
+    for line in input_file:
+        words = line.split()
+        for word in words:
+            word = word.lower()
+        # Special case if we're seeing this word for the first time.
+            if word not in word_count:
+                word_count[word] = 1
+            else:
+                word_count[word] = word_count[word] + 1
+    input_file.close()  # Not strictly required, but good form.
+    return word_count
+
+
+def print_words(filename):
+    word_count = word_count_dict(filename)
+    words = sorted(word_count.keys())
+    for word in words:
+        print(word, word_count[word])
+
+
+def get_count(word_count_tuple):
+    return word_count_tuple[1]
+
+
+def print_top(filename):
+    word_count = word_count_dict(filename)
+
+    # Each item is a (word, count) tuple.
+    # Sort them so the big counts are first using key=get_count() to extract count.
+    items = sorted(word_count.items(), key=get_count, reverse=True)
+
+    # Print the first 20
+    for item in items[:20]:
+        print(item[0], item[1])
+
+
 def main():
     if len(sys.argv) != 3:
         print 'usage: python wordcount.py {--count | --topcount} file'
